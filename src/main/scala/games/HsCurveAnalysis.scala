31b2824053013d9ce5cb2d6e8e87fc5225a99ffd
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-09-01 15:45:37 +0200

    simplistic genetic algo to smooth curves

--- src/main/scala/games/HsCurveAnalysis.scala
+++ src/main/scala/games/HsCurveAnalysis.scala
@@ -1,24 +1,47 @@
 package games
 
 import grizzled.slf4j.Logging
+import scala.util.Random
 
 object HsCurveAnalysis extends App with Logging {
-  type Curve = Map[Int, Int]
+  type Curve = Map[Int, Float]
   type Result = Int
   type Card = Int
-  val sampleCurve = Map(1 -> 6, 2 -> 6, 3 -> 6, 4 -> 6, 5 -> 4).withDefaultValue(0)
   case class Simulation(curve: Curve, turn: Int)
 
-  val sampleSim = Simulation(sampleCurve, 7)
+  implicit class CurveOps(curve: Curve) {
+    def +(other: Curve): Curve =
+      (for (i <- 0 to 10) yield i -> (curve(i) + other(i))).toMap
 
-  def averageCost(sim: Simulation, repeats: Int = 10000): Double = {
+    def breed(other: Curve) =
+      (curve + other).mutate.normalized
+
+    def mutate: Curve =
+      (for (i <- 0 to 10)
+        yield i ->
+        (curve(i) + (Random.nextInt(20) - 10) / 6)).toMap
+
+    def normalized: Curve =
+      (for (i <- 0 to 10)
+        yield i ->
+        curve(i) * 30 / total).toMap
+
+    def avgMiss(turn: Int): Double =
+      averageMiss(Simulation(curve, turn), 100)
+
+    def total = curve.values.sum
+
+    def print: String = (for (i <- curve.keys.min to curve.keys.max) yield s"$i : " + "*" * curve(i).toInt).mkString("\n")
+  }
+
+  def averageMiss(sim: Simulation, repeats: Int = 10000): Double = {
     List.fill(repeats)(play(sim)).sum / repeats.toDouble
   }
 
   def play(sim: Simulation): Result = {
     val initial = util.Random.shuffle((for {
       (cost, count) <- sim.curve
-    } yield List.fill(count)(cost)).flatten.toList)
+    } yield List.fill(count.toInt)(cost)).flatten.toList)
     debug(initial.toString)
 
     val initialSize = 3
@@ -44,6 +67,36 @@ object HsCurveAnalysis extends App with Logging {
     }
   }
 
-  val cost = averageCost(sampleSim)
-  println(f"avg cost $cost%.1f")
+  def nextGen1(selected: List[Curve]): Curve = {
+    val f = selected(Random.nextInt(selected.size))
+    val m = selected(Random.nextInt(selected.size))
+    f.breed(m)
+  }
+
+  def nextGen(selected: List[Curve]): List[Curve] = {
+    selected ::: List.fill(poolSize - selected.size)(nextGen1(selected))
+  }
+
+  def select(curves: List[Curve]) =
+    curves.sortBy(_.avgMiss(keyTurn)).dropRight(6)
+
+  val keyTurn = 6
+  val sampleCurve = Map[Int, Float](1 -> 6, 2 -> 6, 3 -> 6, 4 -> 6, 5 -> 4, 6 -> 2, 7 -> 2).withDefaultValue(0F)
+  val sampleSim = Simulation(sampleCurve, keyTurn)
+  val cost = averageMiss(sampleSim)
+  println(sampleCurve.print)
+  println(f"avg miss $cost%.1f")
+
+  val poolSize = 20
+  println(sampleCurve.avgMiss(keyTurn))
+
+  var curves = List.fill(poolSize)(sampleCurve.mutate)
+  for (i <- 1 to 100) {
+    val selected = curves.sortBy(_.avgMiss(keyTurn)).dropRight(poolSize * 3 / 4)
+    curves = nextGen(selected)
+    println(selected.head.print)
+  }
+  val best = curves.head
+  println(best.avgMiss(keyTurn))
+  println(best.print)
 }
