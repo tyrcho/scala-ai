77c85a663a4bbad856b7ad4822c0e9db3a3f8909
Michel Daviot <michel@daviot.info>
2015-11-16 08:40:31 +0100

    back to ints for curve values

--- src/main/scala/games/HsCurveAnalysis.scala
+++ src/main/scala/games/HsCurveAnalysis.scala
@@ -2,32 +2,45 @@ package games
 
 import grizzled.slf4j.Logging
 import scala.util.Random
+import scala.collection.parallel.ParSeq
 
 object HsCurveAnalysis extends App with Logging {
-  type Curve = Map[Int, Float]
+  type Curve = Map[Int, Int]
   type Result = Int
   type Card = Int
   case class Simulation(curve: Curve, turn: Int)
 
   implicit class CurveOps(curve: Curve) {
     def +(other: Curve): Curve =
-      (for (i <- 0 to 10) yield i -> (curve(i) + other(i))).toMap
+      (for {
+        i <- 0 to 10
+        c = curve(i)
+        o = other(i)
+        newCount = (c + o + 1) / 2
+      } yield i -> newCount).toMap
 
     def breed(other: Curve) =
-      (curve + other).mutate.normalized
+      (curve + other).mutate.norm
 
-    def mutate: Curve =
-      (for (i <- 0 to 10)
-        yield i ->
-        (curve(i) + (Random.nextInt(20) - 10) / 6)).toMap
+    def mutate: Curve = {
+      val temp = curve.mapValues(_ + 2)
+      val cards = temp.deck
+      val more = cards.head
+      val less = cards.find(_ != more).get
+      curve.updated(more, curve(more) + 1).updated(less, curve(less) - 1)
+    }
 
-    def normalized: Curve =
-      (for (i <- 0 to 10)
-        yield i ->
-        curve(i) * 30 / total).toMap
+    def norm: Curve =
+      if (curve.values.sum > 30)
+        (curve - deck.head).norm.withDefaultValue(0)
+      else curve
 
-    def avgMiss(turn: Int): Double =
-      averageMiss(Simulation(curve, turn), 100)
+    def deck: List[Card] = util.Random.shuffle((for {
+      (cost, count) <- curve
+    } yield List.fill(count.toInt)(cost)).flatten.toList)
+
+    def avgMiss(turn: Int, repeats: Int = 1000): Double =
+      averageMiss(Simulation(curve, turn), repeats)
 
     val total = curve.values.sum
 
@@ -35,19 +48,19 @@ object HsCurveAnalysis extends App with Logging {
   }
 
   def averageMiss(sim: Simulation, repeats: Int = 1000): Double = {
-    List.fill(repeats)(play(sim)).sum / repeats.toDouble
+    List.fill(repeats)(play(sim).toLong).sum / repeats.toDouble
   }
 
   def play(sim: Simulation): Result = {
-    val initial = util.Random.shuffle((for {
-      (cost, count) <- sim.curve
-    } yield List.fill(count.toInt)(cost)).flatten.toList)
+    val initial = sim.curve.deck
     debug(initial.toString)
 
     val coin = util.Random.nextBoolean
     val initialSize = 3 + (if (coin) 1 else 0)
     val hand = initial.take(initialSize)
-    playTurn(hand, initial.drop(initialSize), 1, sim.turn, 0, coin)
+    val delta = playTurn(hand, initial.drop(initialSize), 1, sim.turn, 0, coin)
+    debug(s"$delta for $sim")
+    delta
   }
 
   def playTurn(hand: List[Card], deck: List[Card], turn: Int, max: Int, res: Result, hasCoin: Boolean): Result =
@@ -55,9 +68,10 @@ object HsCurveAnalysis extends App with Logging {
       val newHand = deck.head :: hand
       val played = bestCards(newHand, turn, Nil, hasCoin)
       val usedMana = played.sum
-      val stillCoin = hasCoin && usedMana <= turn
+      val usedCoin = usedMana > turn
+      val stillCoin = hasCoin && !usedCoin
       debug(s"turn $turn : hand $newHand | played $played")
-      val cost = turn - usedMana
+      val cost = turn - usedMana + (if (usedCoin) 1 else 0)
       playTurn(newHand diff played, deck.tail, turn + 1, max, res + cost, stillCoin)
     }
 
@@ -93,8 +107,8 @@ object HsCurveAnalysis extends App with Logging {
   def select(curves: List[Curve]) =
     curves.sortBy(_.avgMiss(keyTurn)).dropRight(6)
 
-  val keyTurn = 6
-  val sampleCurve = Map[Int, Float](1 -> 6, 2 -> 6, 3 -> 6, 4 -> 6, 5 -> 4, 6 -> 2, 7 -> 2).withDefaultValue(0F)
+  val keyTurn = 7
+  val sampleCurve = Map[Int, Int](1 -> 4, 2 -> 6, 3 -> 6, 4 -> 6, 5 -> 4, 6 -> 2, 7 -> 2).withDefaultValue(0)
   val sampleSim = Simulation(sampleCurve, keyTurn)
   val cost = averageMiss(sampleSim)
   println(sampleCurve.print)
@@ -102,12 +116,15 @@ object HsCurveAnalysis extends App with Logging {
 
   val poolSize = 20
   println(sampleCurve.avgMiss(keyTurn))
+  var generation = 1
 
   var curves = List.fill(poolSize)(sampleCurve.mutate)
-  for (i <- 1 to 100) {
-    val selected = curves.sortBy(_.avgMiss(keyTurn)).dropRight(poolSize * 3 / 4)
+  for (i <- 1 to 1000) {
+    val selected = curves.sortBy(_.avgMiss(keyTurn, 30 * generation)).dropRight(poolSize * 3 / 4)
     curves = nextGen(selected)
     println(selected.head.print)
+    println(generation + " => " + selected.head.avgMiss(keyTurn))
+    generation += 1
   }
   val best = curves.head
   println(best.avgMiss(keyTurn))
