4666404ee93ee5f78bf6eed5237291a03bc4b961
Michel Daviot <michel@daviot.info>
2016-10-01 14:22:25 +0200

    useful for CG

---
+++ src/main/scala/CodingGameTemplate.scala
@@ -0,0 +1,17 @@
+import math._
+import scala.util._
+import scala.io.StdIn._
+import scala.annotation.tailrec
+
+object CodingGameTemplate {
+  implicit class MinMaxOptionIterable[T](it: Iterable[T]) {
+    def minOption[O: Ordering](by: T => O): Option[T] =
+      if (it.isEmpty) None
+      else Some(it.minBy(by))
+  }
+
+  def sqr(x: Double) = x * x
+
+  def debug(m: => Any) = Console.err.println(m)
+
+}
